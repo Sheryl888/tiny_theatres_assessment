28d42b7a6b5c7ad10d3334533c307f8571408ab7
Sheryl888 <sherylmlange@gmail.com>
2022-03-20 14:54:30 -0500

    12 queries completed

--- rcttc-data.sql
+++ rcttc-data.sql
@@ -7,6 +7,8 @@ select distinct theater from temp_tiny_theatre;
 select distinct theater, theater_address, theater_phone, theater_email 
 from temp_tiny_theatre;
 
+select * from theatre;
+
 insert into theatre (name, address, phone, email)
 select distinct theater, theater_address, theater_phone, theater_email 
 from temp_tiny_theatre;
@@ -20,7 +22,7 @@ from temp_tiny_theatre;
 
 select * from customer;
 
-select distinct date, `show`
+select distinct show_date, `show`   -- ***************************
 from temp_tiny_theatre;
 
 insert into `show` (show_date, show_title, theatre_id)
@@ -30,19 +32,86 @@ join theatre t on tt.theater = t.name;
 
 select * from `show`;
 
-select distinct seat, ticket_price
+select distinct seat, ticket_price  -- ********************the ticket-id is messed up********************************
 from temp_tiny_theatre;
 
-insert into ticket (seat, ticket_price, show_id, customer_id)
-select distinct seat, ticket_price, s.show_id, c.customer_id
+insert into ticket (seat, ticket_price, show_id, customer_id)  -- *******how do you RE_SET the ticket_id if it is a primary key??
+select seat, ticket_price, s.show_id, c.customer_id
 from temp_tiny_theatre tt
-join `show` s on tt.`show` = s.show_title
+join `show` s on tt.`show` = s.show_title and s.show_date = tt.date
 join customer c on tt.customer_email = c.email;
 
+set sql_safe_updates = 0;
+delete from ticket;
+set sql_safe_updates = 1;
+
+-- *****************************UPDATES*********************************************************
 select * from ticket;
 
+-- The Little Fitz's 2021-03-01 performance of The Sky Lit Up is listed with a $20 ticket price. 
+-- The actual price is $22.25 because of a visiting celebrity actor. (Customers were notified.) 
+-- Update the ticket price for that performance only.
+update ticket set
+ticket_price = 22.50
+where show_id = 5;
 
 
+-- In the Little Fitz's 2021-03-01 performance of The Sky Lit Up, Pooh Bedburrow and Cullen Guirau 
+-- seat reservations aren't in the same row. Adjust seating so all groups are seated together in a row. 
+-- This may require updates to all reservations for that performance. Confirm that no seat is double-booked
+-- and that everyone who has a ticket is as close to their original seat as possible.
+
+-- c.ids 37 & 38   show id 5 2021-03-01
+select * from ticket;
+select * from `show`;
+
+ 
+update ticket set
+seat = 'B4'
+where ticket_id = 98;
+
+
+update ticket set
+seat = 'A4'
+where ticket_id = 101;
+
+
+update ticket set
+seat = 'C2'
+where ticket_id = 100;
+
+-- Update Jammie Swindles's phone number from "801-514-8648" to "1-801-EAT-CAKE".
+update customer c set
+c.phone = "1-801-EAT-CAKE"
+where c.customer_id = 48;
+
+-- ****************************DELETES*****************************************************************
+
+-- Delete all single-ticket reservations at the 10 Pin. (You don't have to do it with one query.)
+select * from ticket;
+
+delete from ticket
+where show_id = 1 and customer_id = 7;
+delete from ticket
+where show_id = 2 and customer_id = 8;
+delete from ticket
+where show_id = 2 and customer_id = 10;
+delete from ticket
+where show_id = 2 and customer_id = 15;
+delete from ticket
+where show_id = 3 and customer_id = 18;
+delete from ticket
+where show_id = 3 and customer_id = 19;
+delete from ticket
+where show_id = 3 and customer_id = 22;
+delete from ticket
+where show_id = 3 and customer_id = 25;
+delete from ticket
+where show_id = 4 and customer_id = 26;
+
+-- Delete the customer Liv Egle of Germany. It appears their reservations were an elaborate joke.
+delete from ticket where customer_id = 65;
+delete from customer where customer_id = 65;
 
 
 
--- rcttc-queries.sql
+++ rcttc-queries.sql
@@ -1,12 +1,12 @@
 use tiny_theatre_db;
 
 --  1. Find all performances in the last quarter of 2021 **************************************************************
--- (Oct. 1, 2021 - Dec. 31 2021).  **there should be 5 shows -- this returns 0 rows 
+-- (Oct. 1, 2021 - Dec. 31 2021).  **there should be 3 shows
 select show_title, show_date
 from `show`
 where show_date >= 2021-10-01  -- w/o limit - this was bringing back earlier shows than october 2021
 order by show_date desc
-limit 5;   -- this is not a good solution. I knew there were no shows in 2022, and I knew that there were only 5 shows.
+limit 3;   -- this is not a good solution. I knew there were no shows in 2022, and I knew that there were only 3 shows.
 -- it doesn't want to work with:  where show_date between 2021-10-01 and 2021-12-31;
 
 
@@ -34,18 +34,19 @@ join ticket t using (customer_id)
 join `show` s using (show_id);
 
 --  6. List customer, show, theater, and seat number in one query.
-select concat(c.first_name, c.last_name) customer, s.show_title `show`, th.name theatre, t.seat seat_number
+select concat(c.first_name, " ", c.last_name) customer, s.show_title `show`, th.name theatre, t.seat seat_number
 from customer c
 join ticket t using (customer_id)
 join `show` s using (show_id)
-join theatre th using (theatre_id);
+join theatre th using (theatre_id)
+order by c.last_name desc;
 
 --  7. Find customers without an address. 
 select * from customer
 where address = '';
 
 --  8. Recreate the spreadsheet data with a single query.
--- select * from customer, `show`, theatre, ticket;      *this brought back over 1000*
+-- select * from customer, `show`, theatre, ticket; =======> *this brought back over 1000*
 select c.first_name, c.last_name, c.address, c.phone, c.email,  -- THIS ONE WORKED!
 	s.show_title, s.show_date,
     th.name, th.address, th.phone, th.email,
@@ -55,14 +56,27 @@ join ticket t using (customer_id)
 join `show` s using (show_id)
 join theatre th using (theatre_id);
 
---  9. Count total tickets purchased per customer.
-select 
-sum(ticket_id) total_tickets_sold,
+--  9. Count total tickets purchased per customer. 
+-- *****THis got screwed up as I tried to change things to make #12 work.
+-- it now had added ALL THE TICKETS_IDs for ALL THE TIMES I've loaded the data...even though I deleted first.
+
+-- select 
+-- sum(ticket_id) total_tickets_sold,
+-- concat(c.first_name, " ", c.last_name) customer
+-- from ticket t 
+-- join customer c using (customer_id)
+-- group by concat(c.first_name, " ", c.last_name)
+-- order by sum(ticket_id), concat(c.first_name, " ", c.last_name) desc;
+
+-- **************RE-thought what I really needed returned*************************
+select
+count(t.seat) total_tickets_sold, 
 concat(c.first_name, " ", c.last_name) customer
-from ticket t 
+from ticket t
 join customer c using (customer_id)
 group by concat(c.first_name, " ", c.last_name)
-order by sum(ticket_id), concat(c.first_name, " ", c.last_name) desc;
+order by count(seat), concat(c.first_name, " ", c.last_name) desc;
+
     
 -- 10. Calculate the total revenue per show based on tickets sold.
 select
@@ -82,14 +96,12 @@ join ticket t using (show_id)
 group by th.name;
 
 
--- 12. Who is the biggest supporter of RCTTC? Who spent the most in 2021?
+-- 12. Who is the biggest supporter of RCTTC? Who spent the most in 2021?  Should be Jammie Swindles 220.00
 select 
-concat(c.first_name, " ", c.last_name) customer,
-sum(ticket_id) total_tickets_sold,
-s.show_date as date
-from ticket t
-join customer c using (customer_id)
-join `show` s using (show_id)
-where s.show_date >= 2021-01-01 <= 2021-12-31  
-group by concat(c.first_name, " ", c.last_name), s.show_date
-order by sum(ticket_id) desc;
+concat(c.first_name, " ", c.last_name) customer, 
+sum(t.ticket_price) total_cost
+from customer c
+join ticket t using (customer_id)
+group by concat(c.first_name, " ", c.last_name)
+order by sum(ticket_price) desc;
+
